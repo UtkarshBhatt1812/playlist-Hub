bc6c95f32c859ef4e9e2c005e35e8edab0bbca59
UtkarshBhatt1812 <utkarsh.ub.bhatt1990@gmail.com>
2026-02-25 20:52:30 +0530

    Reading and testing spotify api

--- frontend/src/components/Navbar/profile.tsx
+++ frontend/src/components/Navbar/profile.tsx
@@ -1,8 +1,16 @@
 import { User } from "lucide-react";
+import api from "../../services/api";
+
+
+const onProfileClick = async ()=>{
+  const res = await api.get('/')
+  console.log(res)
+
+}
 const Profile: React.FC = () => {
   return (
     <div className="flex h-10 w-10 items-center justify-center
-     rounded-full border bg-white hover:cursor-pointer">
+     rounded-full border bg-white hover:cursor-pointer " onClick={onProfileClick}>
       <User className="h-6 w-6 text-neutral-500" />
     </div>
   );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,9 +5,13 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+   server: {
+    host: true
+  },
   plugins: [
     tailwindcss(),
-    
+
+ 
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
