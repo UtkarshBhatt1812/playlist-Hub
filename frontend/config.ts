cb4dfa1c7910761e195d054951c4943aab944726
UtkarshBhatt1812 <utkarsh.ub.bhatt1990@gmail.com>
2026-03-31 19:48:34 +0530

    fix: update default API base URL and modify title in index.html

--- frontend/config.ts
+++ frontend/config.ts
@@ -1,5 +1,4 @@
-// const defaultApiBaseUrl = "http://127.0.0.1:3000";
-const defaultApiBaseUrl = "https://spotify.parasnauriyal.com";
+const defaultApiBaseUrl = "http://127.0.0.1:3000";
 
 export const BASE_URL =
   (import.meta.env.VITE_API_BASE_URL as string | undefined)?.trim() ||
